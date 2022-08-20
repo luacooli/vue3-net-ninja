bb17237e82458941a7f350c9fdd119a1b19effeb
Luana <lcorreia@vntrx.com>
2022-08-20 20:36:58 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="app"></div>
+  <div class="app">
+    <p>{{ name }} - {{ age }}</p>
+    <button @click="changeName()">change name</button>
+    <button @click="changeAge()">change age</button>
+  </div>
 </template>
 
 <script lang="ts">
@@ -8,7 +12,29 @@ import { defineComponent, reactive, ref, toRefs } from "vue";
 export default defineComponent({
   name: "App",
   components: {},
-  setup() {},
+  setup() {
+    // ::::: reactive ::::: 
+    // const state = reactive({
+    //   name: "Link",
+    //   age: 25 as number | string,
+    // });
+
+    // return { ...toRefs(state) };
+
+    // ::::: reference 
+    const name = ref("Link");
+    const age = ref<number | string>(25);
+
+    return { name, age };
+  },
+  methods: {
+    changeName(name: string) {
+      this.name = name;
+    },
+    changeAge(age: string | number) {
+      this.age = age;
+    },
+  },
 });
 </script>
 
